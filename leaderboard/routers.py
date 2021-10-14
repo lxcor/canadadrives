3a19fe52a1ffc5eb636ca3b1a029bb4565f07c7f
Alex Alves <frontdesk@lxcor.com>
2021-10-14 14:08:37 -0700

    commit

--- leaderboard/routers.py
+++ leaderboard/routers.py
@@ -3,7 +3,5 @@ from rest_framework import routers
 from leaderboard.views import UserViewSet
 
 
-
-
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', UserViewSet)\
